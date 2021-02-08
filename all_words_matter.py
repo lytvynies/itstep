cfba5b36e49e01009d028529044fea37be830f4e
litvinies <ievgen.lytvyn@ukr.net>
2021-02-08 23:30:49 +0200

    first commit

---
+++ all_words_matter.py
@@ -0,0 +1,7 @@
+text = '''It is a long established fact that a reader will be distracted 
+by the readable content of a page when looking at its layout. The 
+point of using Lorem Ipsum is that it has a, as opposed to using , 
+making it look like readable English. Many desktop publishing packages 
+and web page editors now use Lorem Ipsum as their default model text. Sometimes by 
+accident, sometimes on purpose (injected humour and the like).'''
+print(text.title())
